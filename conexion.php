84be1555378792478e04516d61ca5e8584f97a01
Albert Lukmanov <albert.lukmanov99@gmail.com>
2025-12-11 20:36:48 -0300

    mejoro manejo errores conexion

--- conexion.php
+++ conexion.php
@@ -5,14 +5,15 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'stranger_things_db');
 
-$conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-if ($conexion->connect_error) {
+try {
+    $conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $conexion->set_charset('utf8mb4');
+} catch (Exception $e) {
     die(json_encode([
         'exito' => false,
-        'mensaje' => 'Error de conexión: ' . $conexion->connect_error
+        'mensaje' => 'Error de conexión a la base de datos'
     ]));
 }
-
-$conexion->set_charset('utf8mb4');
 
