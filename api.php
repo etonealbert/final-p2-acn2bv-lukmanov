9a1750c706f365727ab907611724ee543c302ac5
Albert Lukmanov <albert.lukmanov99@gmail.com>
2025-12-11 20:34:13 -0300

    agrego headers cors a api

--- api.php
+++ api.php
@@ -1,5 +1,7 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
 require_once 'conexion.php';
 
